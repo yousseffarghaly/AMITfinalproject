a33f67e6abb117996c6cebb238773b3628a03411
yousseffarghaly <92231334+yousseffarghaly@users.noreply.github.com>
2021-10-10 02:52:28 +0200

    project commit

---
+++ Master/Debug/UART.d
@@ -0,0 +1,14 @@
+UART.d UART.o: .././UART.c .././UART.h .././STD_TYPES.h .././BIT_Math.h \
+ .././REG.h .././DIO_interface.h .././BIT_MATH.h
+
+.././UART.h:
+
+.././STD_TYPES.h:
+
+.././BIT_Math.h:
+
+.././REG.h:
+
+.././DIO_interface.h:
+
+.././BIT_MATH.h:
---
+++ Master/Debug/main.d
@@ -0,0 +1,16 @@
+main.d main.o: .././main.c .././SPI.h .././STD_TYPES.h .././BIT_Math.h \
+ .././REG.h .././DIO_interface.h .././BIT_MATH.h .././UART.h
+
+.././SPI.h:
+
+.././STD_TYPES.h:
+
+.././BIT_Math.h:
+
+.././REG.h:
+
+.././DIO_interface.h:
+
+.././BIT_MATH.h:
+
+.././UART.h:
