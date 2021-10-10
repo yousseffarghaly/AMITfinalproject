54adcfc98ac997cb670a5453bc1d33b8d619e704
yousseffarghaly <92231334+yousseffarghaly@users.noreply.github.com>
2021-10-10 02:57:08 +0200

    project commit

---
+++ Slave/Debug/SPI.d
@@ -0,0 +1,14 @@
+SPI.d SPI.o: .././SPI.c .././SPI.h .././STD_TYPES.h .././BIT_Math.h \
+ .././REG.h .././DIO_interface.h .././BIT_MATH.h
+
+.././SPI.h:
+
+.././STD_TYPES.h:
+
+.././BIT_Math.h:
+
+.././REG.h:
+
+.././DIO_interface.h:
+
+.././BIT_MATH.h:
---
+++ Slave/Debug/main.d
@@ -0,0 +1,14 @@
+main.d main.o: .././main.c .././SPI.h .././STD_TYPES.h .././BIT_Math.h \
+ .././REG.h .././DIO_interface.h .././BIT_MATH.h
+
+.././SPI.h:
+
+.././STD_TYPES.h:
+
+.././BIT_Math.h:
+
+.././REG.h:
+
+.././DIO_interface.h:
+
+.././BIT_MATH.h:
